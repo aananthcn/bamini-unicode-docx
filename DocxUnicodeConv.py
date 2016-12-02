102f24efc78e45e64a3a1186720a16bb40c10863
Aananth C N <caananth@visteon.com>
2016-12-02 18:49:44 +0530

    An attempt to correct the split-i symbol in Tamil

--- DocxUnicodeConv.py
+++ DocxUnicodeConv.py
@@ -1,4 +1,5 @@
 #! /usr/local/bin/python
+# -*- coding: utf-8 -*-
 #
 # The concept behind this code is taken from page
 # http://stackoverflow.com/questions/17850227/text-replace-in-docx-and-save-the-changed-file-with-python-docx
@@ -13,8 +14,12 @@ from BaminiDict import bamini_dict
 from AmudhamDict import amudham_dict
 from AdhawinTamilDict import adhawintamil_dict 
 
+
+# Global variables
 english_fonts = ["Times New Roman"]
 
+
+# Functions....
 def usage():
     print ""
     print "This tool converts word document (.docx) file with Bamini font to"
@@ -124,6 +129,7 @@ def main():
 
         # Convert multi-run paragraph
         i = ref = 0
+        pending_i_vowel = None
         #for run in p.runs:
         for i in range(runs_len):
             print "\n<run>"
@@ -139,8 +145,23 @@ def main():
                 runs[i+1].clear() # clear the content of this run
                 i += 1
 
-            # Convert the concatenated run
-            convert_runfont(runs[ref], paragraph_font)
+            # Correct the ¿ ை(  )error by not coverting immediately
+            #if len(runs[ref].text) > 0 and runs[ref].text[0] == '¿':
+            if runs[ref].font.name == "Adhawin-Tamil":
+                print "P E N D I N G   V O W E L   D E T E C T E D"
+                print runs[ref].text + " == ¿".decode("utf-8")
+                print len(runs[ref].text)
+                pending_i_vowel = runs[ref].text
+                runs[ref].clear()
+            # if ¿ symbol was captured, then preppend it to the next run and convert
+            elif pending_i_vowel != None:
+                runs[ref].text = pending_i_vowel+runs[ref].text
+                convert_runfont(runs[ref], paragraph_font)
+                print "P E N D I N G   V O W E L   C O R R E C T E D"
+                pending_i_vowel = None
+            else:
+                convert_runfont(runs[ref], paragraph_font)
+
             print "</run>"
         print "</para>"
             
